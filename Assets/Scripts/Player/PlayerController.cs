6311c1dbcdf449c063ff7b812af6c04bb41c71e8
Drunkiel <drunkielfel@gmail.com>
2022-06-20 10:51:17 +0200

    Updated movement and jumping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float knockout;
+    public float jumpForce;
+    public bool doubleJump;
+
+    public bool onTheGround;
+    public Transform groundTester;
+    public LayerMask layer;
+
+    Rigidbody rgBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rgBody = GetComponent<Rigidbody> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        onTheGround = GetComponent<TriggerController>().isTriggered;
+
+        Movement();
+        Jump();
+    }
+
+    void Movement(){
+        if(Input.GetKey(KeyCode.A) && !onTheGround){
+            transform.Rotate(-0.25f, 0, 0);
+            knockout = -1;
+        }
+
+        if(Input.GetKey(KeyCode.D) && !onTheGround){
+            transform.Rotate(0.25f, 0, 0);
+            knockout = 1;
+        }
+    }
+
+    void Jump(){
+        if(Input.GetKeyDown(KeyCode.W)){
+            rgBody.AddForce(new Vector2(knockout * jumpForce/10, jumpForce));
+        }
     }
 }
