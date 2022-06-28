6fa06720ab02fed89139cae415a037cdad06e6d9
Drunkiel <drunkielfel@gmail.com>
2022-06-28 09:54:39 +0200

    Random map picker

---
+++ Assets/Scripts/Map/MapPicker.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MapPicker : MonoBehaviour
+{
+    public GameObject[] allMaps;
+    public bool[] pickedMap;
+
+    public Transform placeToSpawn;
+
+    void Start()
+    {
+        pickedMap = new bool[allMaps.Length];
+    }
+
+    public void PickMap()
+    {
+        DestroyActualMap();
+
+        int ranNum = (int)Mathf.Round(Random.Range(0, allMaps.Length));
+        Instantiate(allMaps[ranNum], new Vector3(0, -6, 0), Quaternion.Euler(-90, 0, 0), placeToSpawn);
+    }
+
+    void DestroyActualMap()
+    {
+        GameObject map = GameObject.FindGameObjectWithTag("Wall");
+        Destroy(map);
+    }
+}
