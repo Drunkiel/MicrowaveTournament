1074ab81c1edd5c4f90aa289a740c67f40f7bfe5
Drunkiel <drunkielfel@gmail.com>
2022-06-29 17:15:30 +0200

    Balance changes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,20 +4,17 @@ public class PlayerController : MonoBehaviour
 {
     public float knockout;
     public float jumpForce;
-    public bool doubleJump;
 
     public bool playerOne;
 
     public bool onTheGround;
-    public Transform groundTester;
-    public LayerMask layer;
 
     Rigidbody rgBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        rgBody = GetComponent<Rigidbody> ();
+        rgBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -29,36 +26,41 @@ public class PlayerController : MonoBehaviour
         Jump();
     }
 
-    void Movement(){
+    void Movement()
+    {
         //Player one
-        if(Input.GetKey(KeyCode.A) && playerOne && !onTheGround){
-            MovementSequence(-1, -1);
+        if (Input.GetKey(KeyCode.A) && playerOne && !onTheGround)
+        {
+            MovementSequence(-1, -2);
         }
 
-        if(Input.GetKey(KeyCode.D) && playerOne && !onTheGround){
-            MovementSequence(1, 1);
+        if (Input.GetKey(KeyCode.D) && playerOne && !onTheGround)
+        {
+            MovementSequence(1, 2);
         }
 
         //Player two
-        if(Input.GetKey(KeyCode.LeftArrow) && !playerOne && !onTheGround)
+        if (Input.GetKey(KeyCode.LeftArrow) && !playerOne && !onTheGround)
         {
-            MovementSequence(1, -1);
+            MovementSequence(1, -2);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) && !playerOne && !onTheGround)
         {
-            MovementSequence(-1, 1);
+            MovementSequence(-1, 2);
         }
     }
 
+    //Num1 rotating, Num2 movement speed
     void MovementSequence(int num1, int num2)
     {
         transform.Rotate(0.25f * num1, 0, 0);
         knockout = 1 * num2;
     }
 
-    void Jump(){
-        if(Input.GetKeyDown(KeyCode.W) && playerOne)
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && playerOne)
         {
             JumpSequence();
         }
