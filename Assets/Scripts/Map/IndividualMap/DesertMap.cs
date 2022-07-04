43aa0bd9dde8ee01b8821e5ec3a3cd46b1e0af19
Drunkiel <drunkielfel@gmail.com>
2022-07-04 16:29:11 +0200

    Added animation and changes how drawing works

--- Assets/Scripts/Map/IndividualMap/DesertMap.cs
+++ Assets/Scripts/Map/IndividualMap/DesertMap.cs
@@ -6,18 +6,20 @@ public class DesertMap : MonoBehaviour
     public float windSpeed;
     public int multiplier;
     public int oldMultiplier;
+    public int previousMultiplier;
+    public bool isParticle;
 
     public float cooldown;
     public float resCooldown;
 
     public GameObject[] players;
+    public GameObject windParticle;
     public Rigidbody ball;
 
     // Start is called before the first frame update
     void Start()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
-        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Rigidbody>();
         oldMultiplier = GameObject.FindGameObjectWithTag("MainCamera").transform.GetChild(0).GetComponent<ScoreController>().num;
     }
 
@@ -42,7 +44,7 @@ public class DesertMap : MonoBehaviour
     {
         float num = Mathf.Round(Random.Range(-1, 1));
 
-        if (num >= -0.5f && num <= 0.5f)
+        if ((num >= -0.5f && num <= 0.5f) || previousMultiplier != 0)
         {
             multiplier = 0;
         }
@@ -58,7 +60,10 @@ public class DesertMap : MonoBehaviour
         else
         {
             StartCoroutine("RestTime");
+            ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Rigidbody>();
         }
+
+        previousMultiplier = multiplier;
     }
 
     void Wind()
@@ -70,13 +75,49 @@ public class DesertMap : MonoBehaviour
                 players[i].GetComponent<Rigidbody>().AddForce(new Vector2(windSpeed * multiplier, 4));
             }
 
-            ball.AddForce(new Vector2(windSpeed / 10 * multiplier, 0));
+            WindAnimation();
+
+            ball.AddForce(new Vector2(windSpeed / 10 * multiplier, 0)); // Add windSpeed / 10
         }
+        else
+        {
+            isParticle = false;
+        }
+    }
+
+    void WindAnimation()
+    {
+        if (multiplier == 1 && windParticle.transform.position.x <= 0 && !isParticle)
+        {
+            CreateParticle(multiplier, multiplier);
+            print("1");
+        }
+        else if (multiplier == 1 && windParticle.transform.position.x >= 0 && !isParticle)
+        {
+            CreateParticle(-multiplier, -multiplier);
+            print("2");
+        }
+        else if (multiplier == -1 && windParticle.transform.position.x >= 0 && !isParticle)
+        {
+            CreateParticle(multiplier, multiplier);
+            print("3");
+        }
+        else if (multiplier == -1 && windParticle.transform.position.x <= 0 && !isParticle)
+        {
+            CreateParticle(-multiplier, multiplier);
+            print("4");
+        }
+    }
+
+    void CreateParticle(float num1, float num2)
+    {
+        Instantiate(windParticle, new Vector3(windParticle.transform.position.x * multiplier, windParticle.transform.position.y, windParticle.transform.position.z), Quaternion.Euler(0, 90 * multiplier, 0), this.gameObject.transform);
+        isParticle = true;
     }
 
     private IEnumerator RestTime()
     {
         yield return new WaitForSeconds(resCooldown);
-        multiplier = oldMultiplier;
+        multiplier = -oldMultiplier;
     }
 }
