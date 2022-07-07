9244c8a660d1aa524702a66c721b9e9bb7f2440b
Drunkiel <drunkielfel@gmail.com>
2022-07-07 10:33:53 +0200

    New gate as event

---
+++ Assets/Scripts/PlayGround/Gates/WoodenGateController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class WoodenGateController : MonoBehaviour
+{
+    public int defects;
+    public bool test;
+
+    public bool isTriggered;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTriggered = GetComponent<TriggerController>().isTriggered;
+
+        if (isTriggered)
+        {
+            Injuries();
+        }
+        else
+        {
+            test = false;
+        }
+    }
+
+    void Injuries()
+    {
+        switch (defects)
+        {
+            case 0:
+
+                break;
+
+            case 1:
+                //Change material to yellow
+                break;
+
+            case 2:
+                //Change material to red
+                break;
+
+            case 3:
+                this.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Ball"))
+        {
+            if (!test)
+            {
+                defects++;
+                test = true;
+            }
+        }
+    }
+}
