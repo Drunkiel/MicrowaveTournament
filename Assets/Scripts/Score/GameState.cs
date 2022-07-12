53c136a0ff581bf0fb85e7a5463162a096eea55e
Drunkiel <drunkielfel@gmail.com>
2022-07-12 11:59:24 +0200

    Cutted score controller to smaller scripts

---
+++ Assets/Scripts/Score/GameState.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+[System.Serializable]
+public class GameState
+{
+    public BallController _ballController;
+    public RandomEventController _eventController;
+    public ScoreController _scoreController;
+
+    //To reset
+    public GameObject[] players;
+#nullable enable
+    public GateController[]? _gateControllers;
+    public WoodenGateController[]? _woodenGates;
+#nullable disable
+    public int num;
+    public MapPicker mapPicker;
+
+    public void ResetGate()
+    {
+        GameObject[] gates = _eventController.eventVoids.gates;
+
+        for (int i = 0; i < gates.Length; i++)
+        {
+            if (gates[i].TryGetComponent(out GateController gateController))
+            {
+                _gateControllers[i] = gateController;
+            }
+
+            if (gates[i].TryGetComponent(out WoodenGateController woodenGate))
+            {
+                _woodenGates[i] = woodenGate;
+            }
+        }
+
+        if (_gateControllers[0] != null)
+        {
+            for (int i = 0; i < _gateControllers.Length; i++)
+            {
+                _gateControllers[i].cooldown = _gateControllers[i].resCooldown;
+                _gateControllers[i].actualStep = 0;
+                _gateControllers[i].OpenGate();
+            }
+        }
+
+        if (_woodenGates[0] != null)
+        {
+            for (int i = 0; i < _woodenGates.Length; i++)
+            {
+                _woodenGates[i].defects = 0;
+                _woodenGates[i].Injuries();
+            }
+        }
+    }
+
+    public void ResetLevel()
+    {
+        //Gate reset
+        ResetGate();
+
+        //Reseting players positions and rotation
+        players[0].transform.position = new Vector3(-7, 0, 0);
+        players[1].transform.position = new Vector3(7, 0, 0);
+
+        players[0].transform.rotation = Quaternion.Euler(0, 90, 0);
+        players[1].transform.rotation = Quaternion.Euler(0, -90, 0);
+
+        //Reseting score
+        _scoreController.scoreForPlayerOne = 0;
+        _scoreController.scoreForPlayerTwo = 0;
+
+        //Reseting ball
+        _ballController.ResetBall(num);
+    }
+
+    public void AfterWin()
+    {
+        ResetLevel();
+        _eventController.DrawNumber();
+        _eventController.PickEvent();
+        mapPicker.PickMap();
+
+        _eventController.eventVoids.FindBall();
+        _eventController.eventVoids.FindGates();
+    }
+}
--- Assets/Scripts/Score/ScoreController.cs
+++ Assets/Scripts/Score/ScoreController.cs
@@ -5,32 +5,22 @@ public class ScoreController : MonoBehaviour
 {
     public int scoreForPlayerOne;
     public int scoreForPlayerTwo;
-
     public TMP_Text score;
-    public BallController ballController;
-    public RandomEventController eventController;
 
-    //To reset
-    public GameObject[] players;
-#nullable enable
-    public GateController[]? gateControllers;
-    public WoodenGateController[]? woodenGates;
-#nullable disable
-    public int num;
-    public MapPicker mapPicker;
+    public GameState _gameState;
 
     //To animate
-    SpawnText spawnText;
+    SpawnText _spawnText;
 
     void Start()
     {
-        spawnText = GetComponent<SpawnText>();
+        _spawnText = GetComponent<SpawnText>();
     }
 
     void Update()
     {
         score.text = scoreForPlayerOne.ToString() + " | " + scoreForPlayerTwo.ToString();
-        CheckWinner();
+        if (scoreForPlayerOne >= 4 || scoreForPlayerTwo >= 4) CheckWinner();
     }
 
     public void AddPoints(bool forOne)
@@ -38,101 +28,35 @@ public class ScoreController : MonoBehaviour
         if (forOne)
         {
             scoreForPlayerOne++;
-            num = -1;
-            spawnText.BlueScored();
+            _gameState.num = -1;
+            _spawnText.BlueScored();
         }
         else
         {
             scoreForPlayerTwo++;
-            num = 1;
-            spawnText.RedScored();
+            _gameState.num = 1;
+            _spawnText.RedScored();
         }
 
         //Gate reset
-        ResetGate();
+        _gameState.ResetGate();
 
         //Reset ball after getting point
-        ballController.ResetBall(num);
+        _gameState._ballController.ResetBall(_gameState.num);
     }
 
     void CheckWinner()
     {
         if (scoreForPlayerOne >= 4)
         {
-            num = -1;
+            _gameState.num = -1;
         }
 
         if (scoreForPlayerTwo >= 4)
         {
-            num = 1;
+            _gameState.num = 1;
         }
 
-        if (scoreForPlayerOne >= 4 || scoreForPlayerTwo >= 4)
-        {
-            ResetLevel();
-            eventController.DrawNumber();
-            eventController.PickEvent();
-            mapPicker.PickMap();
-        }
-
-        eventController.eventVoids.FindBall();
-        eventController.eventVoids.FindGates();
-    }
-
-    void ResetGate()
-    {
-        GameObject[] gates = eventController.eventVoids.gates;
-
-        for (int i = 0; i < gates.Length; i++)
-        {
-            if (gates[i].TryGetComponent(out GateController gateController))
-            {
-                gateControllers[i] = gateController;
-            }
-
-            if (gates[i].TryGetComponent(out WoodenGateController woodenGate))
-            {
-                woodenGates[i] = woodenGate;
-            }
-        }
-
-        if (gateControllers[0] != null)
-        {
-            for (int i = 0; i < gateControllers.Length; i++)
-            {
-                gateControllers[i].cooldown = gateControllers[i].resCooldown;
-                gateControllers[i].actualStep = 0;
-                gateControllers[i].OpenGate();
-            }
-        }
-
-        if (woodenGates[0] != null)
-        {
-            for (int i = 0; i < woodenGates.Length; i++)
-            {
-                woodenGates[i].defects = 0;
-                woodenGates[i].Injuries();
-            }
-        }
-    }
-
-    void ResetLevel()
-    {
-        //Gate reset
-        ResetGate();
-
-        //Reseting players positions and rotation
-        players[0].transform.position = new Vector3(-7, 0, 0);
-        players[1].transform.position = new Vector3(7, 0, 0);
-
-        players[0].transform.rotation = Quaternion.Euler(0, 90, 0);
-        players[1].transform.rotation = Quaternion.Euler(0, -90, 0);
-
-        //Reseting score
-        scoreForPlayerOne = 0;
-        scoreForPlayerTwo = 0;
-
-        //Reseting ball
-        ballController.ResetBall(num);
+        _gameState.AfterWin();
     }
 }
