c43822cfa671552e228eddf7cd1dc0ada53e0909
Drunkiel <drunkielfel@gmail.com>
2022-07-17 18:09:19 +0200

    Added charging state and fixed shooting

--- Assets/Scripts/Player/ShootController.cs
+++ Assets/Scripts/Player/ShootController.cs
@@ -1,53 +1,79 @@
 using UnityEngine;
 
-[System.Serializable]
-public class ShootController
+public class ShootController : MonoBehaviour
 {
     public float cooldown;
     private float resCooldown;
-    public bool ballShot;
+    public float ejectionPower;
+    public bool ballShotReady;
+    public bool ballLaunched;
 
     DoorController _doorController;
 
     // Start is called before the first frame update
     void Start()
     {
+        _doorController = GetComponent<DoorController>();
         resCooldown = cooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && ballShotReady)
         {
             Shoot();
         }
 
         CheckStatus();
+        Charge();
     }
 
     void Shoot()
     {
-        if (_doorController.isBallPicked && _doorController.isDoorOpen)
+        if (_doorController.isDoorOpen)
         {
-            ballShot = true;
-            _doorController.ball.GetComponent<Rigidbody>().AddForce(new Vector2(5, 2));
+            ballLaunched = true;
+            ejectionPower = 0;
+            _doorController.ball.GetComponent<Rigidbody>().AddForce(new Vector2(ejectionPower / 2, ejectionPower / 5));
         }
     }
 
     void CheckStatus()
     {
-        if (ballShot)
+        if (ejectionPower >= 3)
+        {
+            ballShotReady = true;
+        }
+        else
+        {
+            ballShotReady = false;
+        }
+
+        if (ballLaunched)
+        {
+            _doorController.isBallPicked = false;
+        }
+
+        if (ballLaunched)
         {
             if (cooldown <= 0)
             {
-                ballShot = false;
+                ballLaunched = false;
                 cooldown = resCooldown;
             }
             else
             {
                 cooldown -= Time.deltaTime;
             }
+        }
+    }
+
+    void Charge()
+    {
+        if (_doorController.isBallPicked && !_doorController.isDoorOpen && ejectionPower <= 10)
+        {
+            ejectionPower += Time.deltaTime * 1.2f;
         }
     }
 }
