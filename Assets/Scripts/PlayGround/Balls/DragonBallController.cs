191eb1f3a5042965253e77c2986888e9e382d58e
Drunkiel <drunkielfel@gmail.com>
2022-07-21 10:24:13 +0200

    Added expliosion that affects to player

--- Assets/Scripts/PlayGround/Balls/DragonBallController.cs
+++ Assets/Scripts/PlayGround/Balls/DragonBallController.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class DragonBallController : MonoBehaviour
 {
+    public bool isTriggered;
+    public LayerMask layer;
+
+    public GameObject particle;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 4);
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        isTriggered = GetComponent<TriggerController>().isTriggered;
+
+        if (isTriggered)
+        {
+            Expliosion();
+        }
+
+        transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+    }
+
+    void Expliosion()
+    {
+        //Creating particle
+        Instantiate(particle, Camera.main.ScreenToWorldPoint(transform.position), Quaternion.identity);
+
+        //Giving damage to player
+        Collider[] hitPlayer = Physics.OverlapSphere(transform.position, 1f, layer);
+
+        foreach (Collider player in hitPlayer)
+        {
+            player.TryGetComponent(out PlayerController playerController);
+            playerController.TakeDamage();
+        }
+
+        //Destroying gameobject
+        Destroy(gameObject);
     }
 }
