24bda5606f4804d2f99288d3c364191e2ea8d86f
Drunkiel <drunkielfel@gmail.com>
2022-07-24 11:50:10 +0200

    Changed how script works

--- Assets/Scripts/Map/MapPicker.cs
+++ Assets/Scripts/Map/MapPicker.cs
@@ -1,23 +1,60 @@
+using System.Linq;
 using UnityEngine;
 
 public class MapPicker : MonoBehaviour
 {
     public GameObject[] allMaps;
-    public bool[] pickedMap;
+    public bool[] pickedMaps;
+    public int[] restOfTheMaps;
 
     public Transform placeToSpawn;
 
     void Start()
     {
-        pickedMap = new bool[allMaps.Length];
+        pickedMaps = new bool[allMaps.Length];
+        Fill();
     }
 
     public void PickMap()
     {
         DestroyActualMap();
 
-        int ranNum = (int)Mathf.Round(Random.Range(0, allMaps.Length));
-        Instantiate(allMaps[ranNum], new Vector3(0, -6, 0), Quaternion.Euler(0, 0, 0), placeToSpawn);
+        //Check if have been any maps left
+        if (restOfTheMaps.Length == 0)
+        {
+            Fill();
+        }
+
+        int ranNum = (int)Mathf.Round(Random.Range(0, restOfTheMaps.Length));
+
+        Instantiate(allMaps[restOfTheMaps[ranNum]], new Vector3(0, -6, 0), Quaternion.Euler(0, 0, 0), placeToSpawn);
+
+        restOfTheMaps = restOfTheMaps.Except(new int[] { restOfTheMaps[ranNum] }).ToArray();
+        ReFillMaps();
+    }
+
+    void ReFillMaps()
+    {
+        for (int i = 0; i < pickedMaps.Length; i++)
+        {
+            pickedMaps[i] = true;
+
+            for (int j = 0; j < restOfTheMaps.Length; j++)
+            {
+                if (i == restOfTheMaps[j]) pickedMaps[i] = false;
+            }
+        }
+    }
+
+    void Fill()
+    {
+        restOfTheMaps = new int[allMaps.Length];
+
+        for (int i = 0; i < restOfTheMaps.Length; i++)
+        {
+            restOfTheMaps[i] = i;
+            pickedMaps[i] = false;
+        }
     }
 
     void DestroyActualMap()
