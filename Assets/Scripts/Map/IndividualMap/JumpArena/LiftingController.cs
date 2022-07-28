77d7d4b744be21bbb2d6acf66f80afd55252fea8
Drunkiel <drunkielfel@gmail.com>
2022-07-28 17:56:43 +0200

    Cutted script

--- Assets/Scripts/Map/IndividualMap/JumpArena/LiftingController.cs
+++ Assets/Scripts/Map/IndividualMap/JumpArena/LiftingController.cs
@@ -3,20 +3,17 @@ using UnityEngine;
 public class LiftingController : MonoBehaviour
 {
     public GameObject[] objectsToLift;
-    public float maxHeightToLift;
-    public float defaultHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        maxHeightToLift = objectsToLift[0].transform.position.y + 1.5f;
         DrawObjectToLift();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Lift(0);
+        objectsToLift[0].GetComponent<FloorFragmentController>().Lift();
     }
 
     void DrawObjectToLift()
@@ -24,14 +21,6 @@ public class LiftingController : MonoBehaviour
         int num = Mathf.RoundToInt(Random.Range(0, objectsToLift.Length));
 
         print(num);
-        Lift(num);
-    }
-
-    void Lift(int num)
-    {
-        while (objectsToLift[num].transform.position.y <= maxHeightToLift)
-        {
-            objectsToLift[num].transform.Translate(Vector3.forward * Time.deltaTime * 4);
-        }
+        objectsToLift[num].GetComponent<FloorFragmentController>().Lift();
     }
 }
