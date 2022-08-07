2a0a0035c18cf85945e43a8d066bcd90afcac230
Drunkiel <drunkielfel@gmail.com>
2022-08-07 10:47:54 +0200

    Upgraded spawning players

--- Assets/Scripts/Network/SpawnPlayers.cs
+++ Assets/Scripts/Network/SpawnPlayers.cs
@@ -3,12 +3,32 @@ using Photon.Pun;
 
 public class SpawnPlayers : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject playerPrefab;
+    public int playersCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.Instantiate(player.name, Vector3.zero, Quaternion.identity);
+        playersCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        SpawnPlayer();
     }
 
+    void SpawnPlayer()
+    {
+        float num = 1;
+
+        if (playersCount == 1)
+        {
+            num = 1;
+        }
+        else if (playersCount == 2)
+        {
+            num = -1;
+        }
+
+        if (playersCount <= 2)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(-7 * num, 0, 0), Quaternion.Euler(0, 90 * num, 0));
+        }
+    }
 }
