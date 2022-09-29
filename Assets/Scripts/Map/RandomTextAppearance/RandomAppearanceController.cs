453e122191bc827f3bfc3995719e3c510761400d
Drunkiel <drunkielfel@gmail.com>
2022-09-29 17:30:42 +0200

    Added randomized text showing

---
+++ Assets/Scripts/Map/RandomTextAppearance/RandomAppearanceController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class RandomAppearanceController : MonoBehaviour
+{
+    public bool isBlueTeam;
+    private float cooldown;
+
+    public GameObject prefab;
+
+    PhotonView view;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown <= 0)
+        {
+            view.RPC("SpawnText", RpcTarget.AllBuffered);
+
+            isBlueTeam = !isBlueTeam;
+            cooldown = Random.Range(5, 10);
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+
+    [PunRPC]
+    void SpawnText()
+    {
+        GameObject[] viewers;
+
+        if (isBlueTeam)
+        {
+            viewers = GameObject.FindGameObjectsWithTag("Viewer_Blue");
+
+            if (viewers.Length > 0) Instantiate(prefab, new Vector3(-8, 0, 10), Quaternion.Euler(0, -25, 0), transform);
+        }
+        else
+        {
+            viewers = GameObject.FindGameObjectsWithTag("Viewer_Red");
+
+            if (viewers.Length > 0) Instantiate(prefab, new Vector3(8, 0, 10), Quaternion.Euler(0, 25, 0), transform);
+        }
+    }
+}
