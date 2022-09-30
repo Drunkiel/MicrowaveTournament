9c6aa86874eb37ca563e69ef30d0f092419eb5b1
Drunkiel <drunkielfel@gmail.com>
2022-09-30 17:48:17 +0200

    Script to control showing UI

---
+++ Assets/Scripts/UI/MainMenu/GameUIController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+    public Toggle menuInfoToggle;
+#nullable enable
+    public GameObject menuInfoObject;
+#nullable disable
+
+    public void ChangeVisibility()
+    {
+        if (menuInfoObject != null)
+        {
+            if (menuInfoToggle.isOn)
+            {
+                menuInfoObject.SetActive(true);
+            }
+            else
+            {
+                menuInfoObject.SetActive(false);
+            }
+        }
+    }
+}
