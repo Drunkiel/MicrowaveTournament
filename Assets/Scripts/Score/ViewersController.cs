fbc61c83854b4759299935edb1b4380cc630823c
Drunkiel <drunkielfel@gmail.com>
2022-12-04 16:55:46 +0100

    Updated viewers

--- Assets/Scripts/Score/ViewersController.cs
+++ Assets/Scripts/Score/ViewersController.cs
@@ -3,36 +3,82 @@ using UnityEngine;
 [System.Serializable]
 public class ViewersController
 {
-    public GameObject[] blueViewers;
-    public GameObject[] redViewers;
+    public GameObject[] blueLayer1;
+    public GameObject[] blueLayer2;
+    public GameObject[] blueLayer3;
+
+    public GameObject[] redLayer1;
+    public GameObject[] redLayer2;
+    public GameObject[] redLayer3;
 
     public ScoreController _scoreController;
 
     public void SpawnViewers()
     {
-        if (_scoreController.PlayerOneWinnedMaps <= blueViewers.Length)
+        switch (_scoreController.PlayerOneWinnedMaps)
         {
-            for (int i = 0; i < _scoreController.PlayerOneWinnedMaps; i++)
-            {
-                blueViewers[i].SetActive(true);
-            }
+            case 1:
+                for (int i = 0; i < blueLayer1.Length; i++)
+                {
+                    blueLayer1[i].SetActive(true);
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < blueLayer2.Length; i++)
+                {
+                    blueLayer2[i].SetActive(true);
+                }
+                break;
+
+            case 3:
+                for (int i = 0; i < blueLayer3.Length; i++)
+                {
+                    blueLayer3[i].SetActive(true);
+                }
+                break;
         }
 
-        if(_scoreController.PlayerTwoWinnedMaps <= redViewers.Length)
+        switch (_scoreController.PlayerTwoWinnedMaps)
         {
-            for (int i = 0; i < _scoreController.PlayerTwoWinnedMaps; i++)
-            {
-                redViewers[i].SetActive(true);
-            }
+            case 1:
+                for (int i = 0; i < redLayer1.Length; i++)
+                {
+                    redLayer1[i].SetActive(true);
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < redLayer2.Length; i++)
+                {
+                    redLayer2[i].SetActive(true);
+                }
+                break;
+
+            case 3:
+                for (int i = 0; i < redLayer3.Length; i++)
+                {
+                    redLayer3[i].SetActive(true);
+                }
+                break;
         }
     }
 
     public void DespawnViewers()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < blueLayer1.Length; i++)
         {
-            blueViewers[i].SetActive(false);
-            redViewers[i].SetActive(false);
+            blueLayer1[i].SetActive(false);
+            blueLayer2[i].SetActive(false);
+            
+            redLayer1[i].SetActive(false);
+            redLayer2[i].SetActive(false);
+        }
+
+        for (int i = 0; i < blueLayer3.Length; i++)
+        {
+            blueLayer3[i].SetActive(false);
+            redLayer3[i].SetActive(false);
         }
     }
 }
